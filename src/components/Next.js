18e0c798affcfbd33abe61d112bfcb75da1f0358
Rams1989 <60887830+Rams1989@users.noreply.github.com>
2022-03-06 21:40:19 +0300

    Delete src directory

--- src/components/Next.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Next.module.css'
-
-const Next = (props) => {
-  return <img className={styles.next} src={props.src} onClick={props.onClick} />
-}
-
-export default Next
